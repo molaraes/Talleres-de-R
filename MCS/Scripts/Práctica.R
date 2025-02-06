87cc2e093bb10963785cc98f060c46c61fb4c068
molaraes <69995017+molaraes@users.noreply.github.com>
2025-02-06 15:37:40 -0600

    Add files via upload

---
+++ MCS/Scripts/Práctica.R
@@ -0,0 +1,18 @@
+# =========================================================
+# Práctica
+# Fecha: 06/02/2024
+# Autora: Mónica Lara Escalante-FLACSO México
+# =========================================================
+
+# De alguna de las bases de datos de socviz:
+# 1. Escoge una variable cuantitativa y calcula:
+# Medidas de tendencia central
+# Medidas de dispersión
+# Mínimo y máximo
+# 2. Elabora un histograma de la variable que escogiste en el punto 1
+# 3. Escoge una variable cualitativa y elabora:
+# Una tabla de frecuencias absolutas y relativas
+# Un gráfico de barras
+# 4. Elabora un diagrama de dispersión con dos variables cuantitativas.
+# 5. Elabora un gráfico de barras con dos variables cualitativas.
+# 6. Elabora un diagrama de caja y bigotes de una variable cuantitativa agrupada por una variable cualitativa.
