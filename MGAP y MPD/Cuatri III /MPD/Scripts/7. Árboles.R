e31d8d628e1e0ebc75c63b9562aedf140c1c3776
molaraes <69995017+molaraes@users.noreply.github.com>
2025-06-23 08:08:36 -0600

    Update 7. Árboles.R

--- MGAP y MPD/Cuatri III /MPD/Scripts/7. Árboles.R
+++ MGAP y MPD/Cuatri III /MPD/Scripts/7. Árboles.R
@@ -67,27 +67,20 @@ base_modif$depression <- factor(base_modif$depression, levels = c(0,1))
 tree.depression <- tree(depression ~ ., base_modif)
 summary(tree.depression)
 
-#De todas las variables disponibles, el algoritmo solo seleccionó academic_pressure para construir el árbol
-#Esto indica que esta variable es la más informativa para predecir depresión según el criterio del algoritmo
-#El árbol tiene 3 nodos terminales (hojas): esto significa que clasifica las observaciones en 3 grupos finales basándose en diferentes niveles de presión académica
-#Valores más bajos de la devianza indican mejor ajuste (desviación total residual/grados de libertad)
-
-#Usamos la función plot() para mostrar la estructura de árbol y  la función text() para mostrar las etiquetas de los  nodos. El argumento pretty = 0 le indica a R que  incluya los nombres de categoría para cualquier predictor cualitativo, en lugar de simplemente mostraruna letra para cada categoría.
+#Usamos la función plot() para mostrar la estructura de árbol y  la función text() para mostrar las etiquetas de los  nodos. 
+#El argumento pretty = 0 le indica a R que  incluya los nombres de categoría para cualquier predictor cualitativo, en lugar de simplemente mostraruna letra para cada categoría.
 
 plot(tree.depression)
 text(tree.depression, pretty=0)
 
-#Si solo escribimos el nombre del objeto del árbol, R imprime  la salida correspondiente a cada rama del árbol. R muestra el criterio de división, el número de  observaciones en esa rama, la desviación, la predicción general  para la rama (Sí o No) y la fracción de observaciones en esa rama que toman valores de Sí y No. Las ramas que conducen a los nodos  terminales se indican con asteriscos.
+#Si solo escribimos el nombre del objeto del árbol, R imprime  la salida correspondiente a cada rama del árbol. 
 
 tree.depression
 
 
-
 # Evaluando el modelo -----------------------------------------------------
 
-#Para evaluar correctamente el rendimiento de un árbol de clasificación en estos datos, debemos estimar el error de prueba. Dividimos las observaciones en un conjunto de entrenamiento y un conjunto de prueba, construimos el árbol utilizando el conjunto de entrenamiento y evaluamos su rendimiento en los datos de prueba. 
-
-#La función predict() se puede utilizar para este propósito. En el caso de un árbol de clasificación, el argumento tipo = "clase" indica a R que devuelva la predicción de clase real.
+#Para evaluar dividimos las observaciones en un conjunto de entrenamiento y un conjunto de prueba, construimos el árbol utilizando el conjunto de entrenamiento y evaluamos su rendimiento en los datos de prueba. 
 
 # Paso 1. Número semilla
 set.seed(1) # Para reproducibilidad
@@ -114,38 +107,8 @@ matriz_confusión_arbol <- confusionMatrix(as.factor(pred_arbol),
 as.factor(prueba_arbol$depression), positive = "1")
 print(matriz_confusión_arbol)
 
-
 # Paso 7. Graficamos
 rpart.plot(modelo_arbol)
-
-#Estructura del árbol
-#Nodo raíz (arriba)
-#0.59: Proporción de casos con depresión en todo el dataset (59%)
-#100%: Contiene el 100% de las observaciones
-
-#Regla de división
-#academic_pressure < 3
-#El árbol divide los datos según si la presión académica es menor a 3
-#Esta es la única variable que el algoritmo consideró importante
-
-#Nodos terminales (hojas)
-#Rama izquierda (presión académica < 3):
-  
-#0: Clasificación predicha = 0 (sin depresión)
-#0.28: Solo 28% de este grupo tiene depresión
-#32%: Representa el 32% del total de observaciones
-
-#Rama derecha (presión académica ≥ 3):
-  
-#1: Clasificación predicha = 1 (con depresión)
-#0.73: 73% de este grupo tiene depresión
-#68%: Representa el 68% del total de observaciones
-
-#Interpretación práctica
-#El modelo es muy simple pero revelador:
-  
-#Baja presión académica (< 3): La mayoría NO tiene depresión (72% están bien)
-#Alta presión académica (≥ 3): La mayoría SÍ tiene depresión (73% la padecen)
 
 #¿Qué pasa si quiero modificar algunos hiperparametros?
 
@@ -175,20 +138,17 @@ matriz_confusion_ajustado <- confusionMatrix(as.factor(pred_arbol_ajustado), as.
 print(matriz_confusion_ajustado)
 
 
-# Validación cruzada ------------------------------------------------------
+# Validación  ------------------------------------------------------
 
-#A continuación, consideramos si podar el árbol podría conducir a mejores resultados. La función cv.tree() realiza una validación cruzada para determinar el nivel óptimo de complejidad del árbol; La poda de complejidad de costos se utiliza para seleccionar una secuencia de árboles para su consideración. Usamos el argumento FUN = prune.misclass para indicar que queremos que la tasa de error de clasificación guíe el proceso de poda y validación cruzada, en lugar del valor predeterminado para la función cv.tree(), que es la desviación. La función cv.tree() reporta el número de nodos terminales de cada árbol considerado (tamaño) así como la tasa de error correspondiente y el valor del parámetro costo-complejidad utilizado (k, que corresponde a α en (8.4)).
-
+#A continuación, consideramos si podar el árbol podría conducir a mejores resultados. 
 
 set.seed(7)
 cv.depression <- cv.tree(tree.depression, FUN = prune.misclass)
 names(cv.depression)
 
 #A pesar de su nombre, dev corresponde al número de errores de validación cruzada.
-
-par(mfrow = c(1, 2))
-plot(cv.depression$size, cv.depression$dev, type = "b")
-plot(cv.depression$k, cv.depression$dev, type = "b")
+cv.depression$dev
+cv.depression$k
 
 #Ahora aplicamos la función prune.misclass()
 prune.depr <- prune.misclass(tree.depression, best = 2)
